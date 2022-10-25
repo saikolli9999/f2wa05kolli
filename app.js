56bfe66045eea5d70d196daaf23ff835b553b27b
Sai Kumar Kolli <s548405@nwmissouri.edu>
2022-10-25 18:02:19 -0500

    my data router

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const port = 3000;
+const port = 3001;
 
 var indexRouter = require('./routes/index');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
