0ce47e8d049ecd599b540086ce6a57fbfcc28f08
Sai Kumar Kolli <s548405@nwmissouri.edu>
2022-10-26 15:18:12 -0500

    compute

--- routes/computation.js
+++ routes/computation.js
@@ -7,7 +7,10 @@ router.get('/', function(req, res, next) {
   if(req.query.x!= null && req.query.x!=undefined) {
     x= req.query.x
   }
-    res.send(`Math.cos(${x}) is ${Math.cos(x)}`);
+    res.send(`Math.cos() applied to ${x} is ${Math.cos(x)}`+"<br></br>"
+    +`Math.asin() applied to ${x} is ${Math.asin(x)}`+"<br></br>"
+      +`Math.asinh() applied to ${x} is ${Math.asinh(x)}`);
+    
 });
 
 module.exports = router;
